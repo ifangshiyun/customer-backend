8fc385b50ed2481aad091b576789f793daa6697c
ifangshiyun <411635039@o365.tku.edu.tw>
2025-05-11 20:48:16 +0800

    menu change

--- routes/menus.py
+++ routes/menus.py
@@ -5,14 +5,14 @@ menus_bp = Blueprint('menus', __name__)
 @menus_bp.route("/", methods=["GET"])
 def get_menus():
     return jsonify([
-        { "name": "原味紅豆餅", "price": 15 },
-        { "name": "奶油餅", "price": 15 },
-        { "name": "芝士餅", "price": 15 },
-        { "name": "巧克力餅", "price": 20 },
-        { "name": "OREO奶酥油餅", "price": 20 },
-        { "name": "可可奶酥白餅", "price": 20 },
-        { "name": "紅豆抹醬餅", "price": 20 },
-        { "name": "抹茶麻糬餅", "price": 20 },
-        { "name": "黑芝麻奶酥油餅", "price": 20 },
-        { "name": "珍珠抹奶酥油餅", "price": 20 }
+        { "name": "原味紅豆餅", "price": 15, "imgUrl": "./images/redbean.png" },
+        { "name": "奶油餅", "price": 15, "imgUrl": "./images/cream.png" },
+        { "name": "芝士餅", "price": 15, "imgUrl": "./images/cream.png"  },
+        { "name": "巧克力餅", "price": 20, "imgUrl": "./images/choco.png" },
+        { "name": "OREO奶酥油餅", "price": 20, "imgUrl": "./images/cream.png"  },
+        { "name": "可可奶酥白餅", "price": 20, "imgUrl": "./images/cream.png"  },
+        { "name": "紅豆抹醬餅", "price": 20, "imgUrl": "./images/cream.png"  },
+        { "name": "抹茶麻糬餅", "price": 20, "imgUrl": "./images/cream.png"  },
+        { "name": "黑芝麻奶酥油餅", "price": 20, "imgUrl": "./images/cream.png"  },
+        { "name": "珍珠抹奶酥油餅", "price": 20, "imgUrl": "./images/cream.png"  }
     ])
